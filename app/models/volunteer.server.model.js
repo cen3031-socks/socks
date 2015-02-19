2d1089716ec644f05f12fbd116505b3f50393857
munarriz <munarriz@ufl.edu>
2015-02-19 17:09:20 -0500

    added volunteer model and additional files

---
+++ app/models/volunteer.server.model.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Volunteer Schema
+ */
+var VolunteerSchema = new Schema({
+	name: {
+		type: String,
+		default: '',
+		required: 'Please fill Volunteer name',
+		trim: true
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+    signedIn: {
+        type: Boolean,
+        default: false
+    }
+    minutedVolunteeredInWeek: {
+        type: Number,
+        default: 0
+    }
+    minutesVolunteeredEver: {
+        type: Number,
+        default: 0
+    }
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
+});
+
+mongoose.model('Volunteer', VolunteerSchema);
