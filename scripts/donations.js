12236d6e52a2c05ca0fa52eb1cd890b3411ad74c
Alexander Russ <git@alexanderruss.com>
2015-02-23 02:06:41 -0500

    Combines data generation scripts

--- scripts/donations.js
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mongoimport --db mean-dev --collection donations --type json donations.json --jsonarray
--- scripts/generate-cats.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-./generate-cat-data.py > ./cats-data.json
-mongoimport --db mean-dev --collection cats --type json ./cats-data.json --jsonArray
-
