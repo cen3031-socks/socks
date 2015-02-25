257494b0987926c18d9c9246ba10fe528a4ed50b
Alexander Russ <git@alexanderruss.com>
2015-02-25 04:26:44 -0500

    Finally fixes that volunteer test.

--- end-to-end-tests/volunteers/volunteers.tests.js
+++ end-to-end-tests/volunteers/volunteers.tests.js
@@ -1,31 +1,22 @@
-
 describe('volunteer page', function() {
-    it('should be able to sign in', function() {
-        browser.get('/#!/volunteers/create');
+	it('should be able to sign in', function() {
+
+		browser.get('/#!/volunteers/create');
 
 		/* Sign a volunteer in */
 		element(by.model('name')).sendKeys('Alexander Russ');
 		element(by.css('[type=submit]:first-child')).click();
 
 		/*expect(browser.getCurrentUrl()).toBe(browser.baseUrl + '/#!/volunteers');*/
-
 		browser.get('/#!/volunteers');
 
-		element.all(by.repeater('volunteer in volunteers')).then(function(data) {
-            console.log(data);
-			expect(protractor.promise.all(data).then(function(elements) {
-                for (var i in elements) {
-                    elements[i].evaluate('volunteer.name').then(function(name) {
-                        console.log(name);
-                        if (name === 'Alexander Russ') {
-                            self.isFound = true;
-                        }
-                    });
-                }
-			}));
-			/*.toContain(*/
-			/*jasmine.objectContaining({name: 'Alexander Russ'})*/
-			/*);*/
-		});
-    });
+		expect(element
+					.all(
+						by.repeater('volunteer in volunteers')
+						  .column('volunteer.name'))
+					.map(function(e) { return e.getText(); }))
+			.toContain('Alexander Russ');
+
+
+	});
 });
