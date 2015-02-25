eb9280d894eda52b259200be8975b9cb4541a213
Alexander Russ <git@alexanderruss.com>
2015-02-24 19:16:16 -0500

    Adds test for deleting cats.

--- end-to-end-tests/cats/cats.tests.js
+++ end-to-end-tests/cats/cats.tests.js
@@ -1,11 +1,12 @@
 describe('cats homepage', function() {
     it('should have a title', function() {
-        browser.get('http://localhost:3000');
+        browser.get('/');
 
         expect(browser.getTitle()).toEqual('SOCKS (Save Our Cats and Kittens)');
     });
-    it('should create a cat successfully', function() {
-        browser.get('http://localhost:3000/');
+
+    it('should be able to add a cat to database', function() {
+        browser.get('/');
 
         element(by.id('new-cat')).click();
 
@@ -49,4 +50,64 @@ describe('cats homepage', function() {
 			}
 		}
     });
-})
+
+	describe('with pre-existing data', function() {
+		beforeEach(function(done) {
+			browser.get('/#!/');
+			browser.waitForAngular();
+			var self = this;
+			browser.executeAsyncScript(function(callback) {
+				/* Make sure there's at least one cat in the db */
+				var element = document.querySelector('.cat-grid');
+				var scope = angular.element(element).scope();
+				var newCat = {
+					name: 'Test Cat',
+					color: 'Green',
+					breed: 'Test Breed', 
+					location: 'Test Location',
+					dateOfBirth: '02/15/2014',
+					dateOfArrival: '02/15/2015',
+					vet: 'Dr. Smith',
+					temperament: 'Test Temperament',
+					owner: 'Test Owner',
+					description: 'Test Description',
+					originAddress: '1 Somewhere Street',
+					originPerson: 'Mr. Nobody',
+					notes: ['Test Note']
+				};
+				scope.$apply(function() {
+					for (var attr in newCat) {
+						scope[attr] = newCat[attr];
+					}
+				});
+				scope.create().then(callback);
+			}).then(function(data) {
+				self.createdCatId = data._id;
+			}).then(done);
+		});
+
+		it('should be able to delete cat from database', function() {
+			browser.get('/#!/cats/' + this.createdCatId);
+			
+			/* delete the cat */
+			element(by.id('delete-cat')).click();
+
+			browser.get('/#!/');
+			/* check that we went back to the homepage organically */
+			expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + "/#!/");
+			
+			var self = this;
+			/* check that the cat doesn't exist anymore */
+			element.all(by.repeater('cat in cats')).then(function(cats) {
+				for (var i in cats) {
+					expect(cats[i].evaluate('cat._id')).not.toBe(self.createdCatId);
+				}
+			});
+		});
+
+		it('should be able to add notes to a cat', function() {
+			browser.get('/#!/cats/' + this.createdCatId);
+		});
+	});
+
+});
--- end-to-end-tests/config.js
+++ end-to-end-tests/config.js
@@ -1,4 +1,5 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['cats/cats.tests.js']
+    specs: ['cats/cats.tests.js'],
+	baseUrl: 'http://localhost:3000'
 }
