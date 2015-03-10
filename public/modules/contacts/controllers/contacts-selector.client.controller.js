a7a5d4e9e13b0522d0417187fd8d21daf976b9f8
Alexander Russ <git@alexanderruss.com>
2015-03-10 12:19:56 -0400

    Makes contact selector look better and adds the ability to create a new contact from the selector interface.

--- public/modules/contacts/controllers/contacts-selector.client.controller.js
+++ public/modules/contacts/controllers/contacts-selector.client.controller.js
@@ -2,43 +2,58 @@
 angular.module('contacts')
     .controller('ContactsSelectorController', ['$stateParams', 'Contacts', '$modal', '$scope',
         function($stateParams, Contacts, $modal, $scope) {
-            $scope.contacts = Contacts.query();
             $scope.selectedContacts = [];
 
             $scope.showModal = function() {
                 var modalInstance = $modal.open({
                     templateUrl: '/modules/contacts/views/select-contact-list.client.template.html',
-                    controller: function($scope, $modalInstance, contacts) {
-                        $scope.contacts = contacts;
+                    controller: function($scope, $modalInstance, Contacts, $modal) {
+                        $scope.contacts = Contacts.query();
                         $scope.cancel = function () {
                             $modalInstance.dismiss('cancel');
                         };
                         $scope.select = function(contact) {
                             $modalInstance.close(contact);
                         }
+						$scope.createContact = function() {
+							var createModal = $modal.open({
+								templateUrl: '/modules/contacts/views/create-contacts.client.view.html',
+								size: 'lg',
+								controller: ''
+							});
+							onRouteChangeOff = $scope.$on('$locationChangeStart', function(event, newState, oldState) {
+								if (newState.match(/\/contacts\/[\w\d]{24}$/)) {
+									onRouteChangeOff();
+									createModal.dismiss();
+									$modalInstance.close(Contacts.get({contactId: newState.match(/[\w\d]{24}$/)}));
+									event.preventDefault();
+								}
+							});
+						};
                     },
-                    size: 'lg',
-                    resolve: {
-                        contacts: function() {
-                            return $scope.contacts;
-                        }
-                    }
+                    size: 'md'
                 });
                 modalInstance.result.then(function(contact) {
                     // The contact was selected.
-                    alert(contact.firstName);
+					if ($scope.selectedContacts.indexOf(contact) !== -1) {
+						$scope.selectedContacts.splice($scope.selectedContacts.indexOf(contact), 1);
+					}
+					$scope.selectedContacts.push(contact);
                 }, function() {
                 })
-            }
+            };
+
+			$scope.unselect = function(contact, index) {
+				$scope.selectedContacts.splice(index, 1);
+			};
     }]).directive('contactsSelector',
         function() {
             return {
                 restrict: 'E',
                 controller: 'ContactsSelectorController',
-                scope: { maxContacts: '=max' },
+                scope: { maxContacts: '=max', selectedContacts: '=?ngModel' },
                 templateUrl: '/modules/contacts/views/contact-selector.client.template.html',
-                link: function(scope, element, attrs, ctrl) {
-                }
+                link: function(scope, element, attrs, ctrl) { }
             };
         }
     );
