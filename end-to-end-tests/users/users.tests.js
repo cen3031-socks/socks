c6efcf0107655112d3232f8f5f38a7c610b9473c
Eric Pogash <ericpogash@gmail.com>
2015-04-05 16:01:23 -0400

    Added protractor test for new account creation.

--- end-to-end-tests/users/users.tests.js
+++ end-to-end-tests/users/users.tests.js
@@ -1,44 +1,40 @@
 var request = require('request'),
 	MongoClient = require('mongodb').MongoClient;
 
-describe('contacts listing', function() {
+describe('employees listing', function() {
 	it('should have a title', function() {
 		browser.get('/');
 		expect(browser.getTitle()).toEqual('SOCKS (Save Our Cats and Kittens)');
 	});
 
 	it('should be able to create new employee', function() {
-		browser.get('/#!/contacts');
-		element(by.partialButtonText('New')).click();
-		var contact = {
-			firstName: "John",
-			surname: "Appleseed",
-			address: "1 Infinite Loop",
-			city: "Cupertino",
-			state: "California",
-			zipCode: "12345",
-			phone: "(850) 555 - 1234",
-			email: "john.appleseed@example.com"
+		browser.get('/#!/employees');
+		element(by.css('.myButton')).click();
+		var user = {
+			firstName: "Test",
+			lastName:"Test",
+			email: "email@email.com",
+			permissionLevel: "0"
 		};
 
-        for (var i in contact) {
-            element(by.model(i)).sendKeys(contact[i]);
+        for (var i in user) {
+            element(by.model(i)).sendKeys(user[i]);
         }
-        element(by.buttonText('Save')).click();
+        element(by.buttonText('Create')).click();
 
-		/* Make sure we went to a view contact page */
-		expect(browser.getCurrentUrl()).toMatch(/contacts\/[a-z\d]{24}$/);
+		/* Home page */
+		expect(browser.getCurrentUrl()).toMatch();
 
-		/* check that all the fields have the right values. */
-		for (var i in contact) {
-			if (i === 'firstName' || i === 'surname') {
-				expect(element(by.binding(i)).getText()).toBe(
-						contact['firstName'] + ' ' + contact['surname']
-					);
-			}
-			else {
-				expect(element(by.binding(i)).getText()).toBe(contact[i]);
-			}
-		}
+		/* check that all the fields have the right values. (need edit profile to work) */
+		// for (var i in user) {
+		// 	if (i === 'firstName' || i === 'surname') {
+		// 		expect(element(by.binding(i)).getText()).toBe(
+		// 				contact['firstName'] + ' ' + contact['surname']
+		// 			);
+		// 	}
+		// 	else {
+		// 		expect(element(by.binding(i)).getText()).toBe(contact[i]);
+		// 	}
+		// }
 	});
 });
