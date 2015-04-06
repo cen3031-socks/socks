f7d0e7d058756074b290d4b0f18addccc6c8dafd
Aaorn <asilcott1@gmail.com>
2015-04-06 12:47:01 -0400

    donation backend clean up

--- app/tests/contact.server.model.test.js
+++ app/tests/contact.server.model.test.js
@@ -18,21 +18,12 @@ var user, contact;
  */
 describe('Contact Model Unit Tests:', function() {
 	beforeEach(function(done) {
-		user = new User({
-			firstName: 'Full',
-			lastName: 'Name',
-			displayName: 'Full Name',
-			email: 'test@test.com',
-			username: 'username',
-			password: 'password'
-		});
 
-		user.save(function() { 
-			contact = new Contact({
-				firstName: 'Contact Name',
-				surname: 'lastName'
-			});
+		contact = new Contact({
+			firstName: 'Contact Name',
+			surname: 'lastName'
 		});
+		done();
 	});
 
 	describe('Method Save', function() {
