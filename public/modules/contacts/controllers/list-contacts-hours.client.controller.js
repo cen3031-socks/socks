c00e018b925ab785077ec1cddba7a1cce2a52974
Eric Pogash <ericpogash@gmail.com>
2015-04-21 05:10:21 -0400

    Volunteers are sorted alphabetically; last names added to the list.

--- public/modules/contacts/controllers/list-contacts-hours.client.controller.js
+++ public/modules/contacts/controllers/list-contacts-hours.client.controller.js
@@ -1,8 +1,27 @@
 'use strict';
 var contactsApp = angular.module('contacts');
 
+
+contactsApp.filter('orderObjectBy', function() {
+  return function(items, field, reverse) {
+    var filtered = [];
+    angular.forEach(items, function(item) {
+      filtered.push(item);
+    });
+    filtered.sort(function (a, b) {
+      return (a[field] > b[field] ? 1 : -1);
+    });
+    if(reverse) filtered.reverse();
+    return filtered;
+  };
+})
+
 contactsApp.controller('ContactsHoursController', ['$scope', '$stateParams', 'Authentication', 'Contacts', '$modal', '$log', '$location', 'Dialogs', 'Volunteers',
     function($scope, $stateParams, Authentication, Contacts, $modal, $log, $location, Dialogs, Volunteers) {
+
+    	$scope.makeName = function(firstname, surname) {
+    		return firstname + ' ' + surname;
+    	};
 
         $scope.find = function() {
             $scope.contacts = Contacts.query(function() {
